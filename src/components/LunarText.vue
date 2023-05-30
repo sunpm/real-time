cafe97d50a50307522a858969f1de9096e40881c
sunpm <15022352@qq.com>
2023-05-30 10:46:09 +0800

    修复报错

--- src/components/LunarText.vue
+++ src/components/LunarText.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="lunar-text">
     <div class="lunar-gz-date">
-      <span>{{ solarLunarText.yearCn }}</span>
-      <span>{{ solarLunarText.monthCn }}</span>
-      <span>{{ solarLunarText.dayCn }}</span>
+      <span>{{ solarLunarText?.yearCn }}</span>
+      <span>{{ solarLunarText?.monthCn }}</span>
+      <span>{{ solarLunarText?.dayCn }}</span>
     </div>
     <div class="lunar-gz">
       <!--壬寅年 【虎年】 癸卯月 癸未日-->
-      <span>{{ solarLunarText.gzYear }}年</span>
-      <span>【{{ solarLunarText.animal }}年】</span>
-      <span>{{ solarLunarText.gzMonth }}月</span>
-      <span>{{ solarLunarText.gzDay }}日</span>
+      <span>{{ solarLunarText?.gzYear }}年</span>
+      <span>【{{ solarLunarText?.animal }}年】</span>
+      <span>{{ solarLunarText?.gzMonth }}月</span>
+      <span>{{ solarLunarText?.gzDay }}日</span>
     </div>
   </div>
 </template>
